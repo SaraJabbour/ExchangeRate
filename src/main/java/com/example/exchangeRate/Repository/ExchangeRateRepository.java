2c2e99ad3e3233500c972a1c07d078870b494c09
Zeva <89845369+zeva02@users.noreply.github.com>
2022-11-22 12:21:02 +0200

    Model creation for Api request and response

--- src/main/java/com/example/exchangeRate/Repository/ExchangeRateRepository.java
+++ src/main/java/com/example/exchangeRate/Repository/ExchangeRateRepository.java
@@ -1,9 +1,9 @@
 package com.example.exchangeRate.Repository;
 
-import com.example.exchangeRate.Model.RateOfEurInUsd;
+import com.example.exchangeRate.Model.Rate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExchangeRateRepository extends CrudRepository<RateOfEurInUsd,Long> {
+public interface ExchangeRateRepository extends CrudRepository<Rate,Long> {
 }
--- src/main/java/com/example/exchangeRate/Service/ExchangeRateService.java
+++ src/main/java/com/example/exchangeRate/Service/ExchangeRateService.java
@@ -2,7 +2,7 @@ package com.example.exchangeRate.Service;
 
 import com.example.exchangeRate.Model.ApiResponse;
 import com.example.exchangeRate.Repository.ExchangeRateRepository;
-import okhttp3.Response;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ExchangeRateService {
     private final static String API_KEY="UzajJ80FpVw05bm0LpyuO5LE14P0CoVL";
-    private final static String uri="https://api.exchangeratesapi.io/v1/timeseries?access_key=UzajJ80FpVw05bm0LpyuO5LE14P0CoVL&start_date=2012-05-01&end_date=2012-05-25";
+    private final static String uri="https://api.exchangeratesapi.io/v1/timeseries?access_key={api_key}&start_date=2012-05-01&end_date=2012-05-25";
     @Autowired
     private RestTemplate restTemplate;
     @Autowired
@@ -24,8 +24,19 @@ public class ExchangeRateService {
     //Testing API call response
     public void getExternalData() {
         try {
-            ResponseEntity<String> response = restTemplate.getForObject(uri, ResponseEntity.class);
-            System.out.println(response);
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("apikey", "UzajJ80FpVw05bm0LpyuO5LE14P0CoVL");
+            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+            ResponseEntity<ApiResponse> response = restTemplate.exchange(
+                    "https://api.apilayer.com/exchangerates_data/timeseries?start_date=2022-01-01&end_date=2022-01-05&symbols=USD", HttpMethod.GET, requestEntity, ApiResponse.class);
+            try{
+                JSONObject jsondailyRates=response.getBody().getJsondailyRates();
+                System.out.println(jsondailyRates);
+            }
+            catch(NullPointerException e){
+                e.printStackTrace();
+            }
+
         }
         catch (HttpClientErrorException e){
             System.out.println(e.getMessage());
