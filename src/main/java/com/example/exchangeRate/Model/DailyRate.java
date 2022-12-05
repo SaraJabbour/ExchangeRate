2e2f1f0e11378a6d4a09249a431e7fbe9110cc2b
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-05 19:59:38 +0200

    Changed Id from generated long to the date

--- src/main/java/com/example/exchangeRate/Model/DailyRate.java
+++ src/main/java/com/example/exchangeRate/Model/DailyRate.java
@@ -2,33 +2,22 @@ package com.example.exchangeRate.Model;
 
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
-@Entity(name="dailyRate")
+@Entity(name = "dailyRate")
 public class DailyRate {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private LocalDate date;
     private Double USD;
 
-    public DailyRate(){
+    public DailyRate() {
 
     }
+
     public DailyRate(LocalDate date, Double value) {
-        this.date=date;
-        this.USD=value;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.date = date;
+        this.USD = value;
     }
 
     public LocalDate getDate() {
