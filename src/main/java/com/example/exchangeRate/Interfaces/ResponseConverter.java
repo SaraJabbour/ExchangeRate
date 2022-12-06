3b26db9f772029a1f9aceb6efa6f9ff8b018fb90
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-06 07:36:59 +0200

    Deleted interfaces

--- src/main/java/com/example/exchangeRate/Interfaces/ResponseConverter.java
+++
@@ -1,9 +0,0 @@
-package com.example.exchangeRate.Interfaces;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-
-public interface ResponseConverter {
-    List<?> convertResponse(ResponseEntity<String> responseEntity);
-}
