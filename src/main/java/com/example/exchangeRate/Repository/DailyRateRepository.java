ccfa4b920cb7542fed3d95f6270ec2c861f89450
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-09 09:42:49 +0200

    Created query to get variation between latest two values

--- src/main/java/com/example/exchangeRate/Repository/DailyRateRepository.java
+++ src/main/java/com/example/exchangeRate/Repository/DailyRateRepository.java
@@ -1,9 +1,16 @@
 package com.example.exchangeRate.Repository;
 
-import com.example.exchangeRate.Model.DailyRate;
+import com.example.exchangeRate.Entity.DailyRate;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface DailyRateRepository extends CrudRepository<DailyRate,Long> {
+public interface DailyRateRepository extends CrudRepository<DailyRate, Long> {
+
+    @Query(value = "select usd-lag(usd,1,0) over (order by date) from daily_rate order by date desc limit 1",
+            nativeQuery = true)
+    Optional<Double> getDifferenceBetweenLatestValues();
 }
