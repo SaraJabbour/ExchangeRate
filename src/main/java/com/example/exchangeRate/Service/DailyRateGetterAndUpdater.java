196e750af9f113b93297890a5140e13715323baf
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-09 19:01:52 +0200

    Created class to get daily rate and update its table

---
+++ src/main/java/com/example/exchangeRate/Service/DailyRateGetterAndUpdater.java
@@ -0,0 +1,28 @@
+package com.example.exchangeRate.Service;
+
+import com.example.exchangeRate.Entity.DailyRate;
+import com.example.exchangeRate.Interface.DailyRateGetter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DailyRateGetterAndUpdater {
+    @Autowired
+    private DailyRateService dailyRateService;
+
+    private DailyRateGetter dailyRateGetter;
+
+    @Autowired
+    public DailyRateGetterAndUpdater(@Qualifier("apilayer") ApiLayerDailyRateGetter dailyRateGetter) {
+        this.dailyRateGetter = dailyRateGetter;
+    }
+
+    public void getAndSaveDailyRate() {
+        DailyRate dailyRate = dailyRateGetter.getDailyRate();
+        dailyRateService.saveDailyRate(dailyRate);
+    }
+
+
+}
