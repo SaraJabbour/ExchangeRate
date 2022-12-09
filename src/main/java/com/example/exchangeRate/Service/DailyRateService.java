9e63a35674e465af587f5cb9de16e39a0fb2640e
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-09 09:43:16 +0200

    Updated method to get variation between latest two values

--- src/main/java/com/example/exchangeRate/Service/DailyRateService.java
+++ src/main/java/com/example/exchangeRate/Service/DailyRateService.java
@@ -1,19 +1,21 @@
 package com.example.exchangeRate.Service;
 
-import com.example.exchangeRate.Model.DailyRate;
+import com.example.exchangeRate.Entity.DailyRate;
 import com.example.exchangeRate.Repository.DailyRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DailyRateService {
     @Autowired
     private DailyRateRepository dailyRateRepository;
 
-    public void saveDailyRates(List<DailyRate> dailyRates) throws RuntimeException {
-        dailyRateRepository.saveAll(dailyRates);
+    public void saveDailyRate(DailyRate dailyRate) {
+        dailyRateRepository.save(dailyRate);
     }
 
+    public Double getDifferenceBetweenLatestValues() {
+        return dailyRateRepository.getDifferenceBetweenLatestValues().orElseThrow();
+
+    }
 }
