f18108712936bca84bb6160e1f529dc83e52d1dc
Zeva <89845369+zeva02@users.noreply.github.com>
2022-12-09 09:39:05 +0200

    Added update methods for increases and decreases in rate variation

--- src/main/java/com/example/exchangeRate/Service/RateVariationService.java
+++ src/main/java/com/example/exchangeRate/Service/RateVariationService.java
@@ -1,9 +1,10 @@
 package com.example.exchangeRate.Service;
 
-import com.example.exchangeRate.Model.RateVariation;
+import com.example.exchangeRate.Entity.RateVariation;
 import com.example.exchangeRate.Repository.RateVariationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,12 +13,18 @@ public class RateVariationService {
     @Autowired
     private RateVariationRepository rateVariationRepository;
 
-    public RateVariation getRateVariation(){
-        Optional <RateVariation> optionalRateVariation= rateVariationRepository.findById(1);
-        return optionalRateVariation.orElseThrow ();
-    }
-    public void saveRateVariation(RateVariation rateVariation) throws RuntimeException{
-        rateVariationRepository.save(rateVariation);
+    public RateVariation getRateVariation() {
+        Optional<RateVariation> optionalRateVariation = rateVariationRepository.findById(1);
+        return optionalRateVariation.orElseThrow();
     }
 
+    @Transactional
+    public void updateIncrease() {
+        rateVariationRepository.updateIncrease();
+    }
+
+    @Transactional
+    public void updateDecrease() {
+        rateVariationRepository.updateDecrease();
+    }
 }
