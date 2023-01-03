752f688e788729c31680da18146ec330732d6eac
Zeva <89845369+zeva02@users.noreply.github.com>
2023-01-03 21:32:17 +0200

    Added filter,authentication provider, password encoder

--- src/main/java/com/example/exchangeRate/security/SecurityConfiguration.java
+++ src/main/java/com/example/exchangeRate/security/SecurityConfiguration.java
@@ -1,56 +1,71 @@
 package com.example.exchangeRate.security;
 
+import com.example.exchangeRate.security.jwt.AuthEntryPointJwt;
+import com.example.exchangeRate.security.jwt.AuthTokenFilter;
+import com.example.exchangeRate.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static com.example.exchangeRate.security.ApplicationUserRole.ADMIN;
-import static com.example.exchangeRate.security.ApplicationUserRole.USER;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.DELETE, "/api/v1/dailyrate/{dailyRateId}").hasRole(ADMIN.name()) // Admin should be able to delete
-                .antMatchers(HttpMethod.PUT, "/api/v1/dailyrate/{dailyRateId}").hasRole(ADMIN.name()) // Admin should be able to update
-                .antMatchers(HttpMethod.POST, "/api/v1/dailyrate").hasAnyRole(ADMIN.name()) // Admin should be able to add rate.
-
-                .antMatchers("/api/v1/dailyrate").hasAnyRole(ADMIN.name(), USER.name()) // All 2 users should be able to get all rates.
-                .antMatchers("/api/v1/dailyrate/{date}").hasAnyRole(ADMIN.name(), USER.name()) // All 2 users should be able to get a rate by date
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user =
-                User.withUsername("user")
-                        .password(encoder.encode("password"))
-                        .authorities(USER.getGrantedAuthorities())
-                        .build();
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("password123"))
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/v1/**").permitAll()
+                .anyRequest()
+                .authenticated();
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
     }
 }
