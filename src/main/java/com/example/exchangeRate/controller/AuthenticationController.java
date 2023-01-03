c7fca46b4cb02c1eedd665331169d2af9bbf4765
Zeva <89845369+zeva02@users.noreply.github.com>
2023-01-03 21:35:50 +0200

    Create authentication controller

---
+++ src/main/java/com/example/exchangeRate/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.exchangeRate.controller;
+
+import com.example.exchangeRate.security.payload.request.SignInRequest;
+import com.example.exchangeRate.security.payload.request.SignUpRequest;
+import com.example.exchangeRate.security.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody SignInRequest signInRequest) {
+        return authService.authenticateUser(signInRequest);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        return authService.registerUser(signUpRequest);
+    }
+}
