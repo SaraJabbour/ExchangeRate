c6a7b63f40e3ad885f631a87f893c8ac7a927ca7
Zeva <89845369+zeva02@users.noreply.github.com>
2023-01-04 11:01:34 +0200

    Code cleanup

--- src/main/java/com/example/exchangeRate/controller/RateVariationController.java
+++ src/main/java/com/example/exchangeRate/controller/RateVariationController.java
@@ -3,7 +3,6 @@ package com.example.exchangeRate.controller;
 
 import com.example.exchangeRate.models.RateVariation;
 import com.example.exchangeRate.service.RateVariationService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-@Log4j2
 public class RateVariationController {
     @Autowired
     private RateVariationService exchangeVariationService;
